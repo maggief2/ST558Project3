1b7caa0e698fb2ecf48ebf8f4d3a75d42025812a
mfeng2 <mfeng2@ncsu.edu>
2021-07-26 19:56:25 -0400

    About page and Data page

--- server.R
+++ server.R
@@ -1,26 +1,58 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+library(tidyverse)
+library(DT)
+SeoulBike <- read_csv("SeoulBikeData.csv")
+colnames(SeoulBike) <- c("Date", "Count", "Hour", "Temperature", "Humidity",
+                         "WindSpeed", "Visibility", "DewPoint", "SolarRadiation", "Rainfall", 
+                         "Snowfall", "Seasons", "Holiday", "FunctioningDay")
 
 library(shiny)
 
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
+shinyServer(function(input, output, session) {
+  
+  #Data page - table
+  output$table <- renderTable({
+    #filter rows
+    var <- input$season
+    if (input$var == "All"){
+      data <- as.data.frame(SeoulBike)
+    } else {
+      BikeSub <- SeoulBike[SeoulBike$Seasons == input$var,]
+      data <- as.data.frame(BikeSub)
+    }
     
+    #filter columns
+    col <- input$col
+    if (input$col == "Both"){
+      tab <- data 
+    } else {
+      if (input$col == "Weather"){
+        tab <- data %>% select("Count", "Temperature", "Humidity", "WindSpeed", "Visibility", 
+                               "DewPoint", "SolarRadiation", "Rainfall", "Snowfall")
+      } else {
+        tab <- data %>% select("Count", "Date", "Hour", "Seasons", "Holiday", "FunctioningDay")
+      }
+    }
+    as.data.frame(tab)
+  })
+  
+  #Data page - download button
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste0("data.csv")
+    },
+    content = function(file) {
+      write.csv(SeoulBike, file)
+    }
+  )
+  
+  #Data Exploration page 
+  output$distPlot <- renderPlot({
     # generate bins based on input$bins from ui.R
     x    <- faithful[, 2] 
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
     # draw the histogram with the specified number of bins
     hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
   })
-  
 })
--- ui.R
+++ ui.R
@@ -1,33 +1,69 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+shinyUI(
+  navbarPage("Bike Modeling (Seoul)",
+      tabPanel("About",
+               #Picture related to data
+               img(src='bicycle.png', height = "20%", width = "20%"),
+               
+               #Purpose of App
+               h3("Purpose of the App"),
+               h4("This app will allow the user to explore and model the Seoul bike sharing demand."),
+               
+               #Discussion of data and its source
+               h3("The Data"),
+               h4("The data comes from the UCI machine learning repository and was labeled ",
+               a("Seoul Bike Sharing Demand.", href = "https://archive.ics.uci.edu/ml/datasets/Seoul+Bike+Sharing+Demand"), " The variable of interest is the Rented Bike Count, because it is important that the bikes are available and accessible to the public at the right time to reduce waiting time. The remaining 13 varables in this dataset are: Date, Hour, Temperature, Humidity, Wind speed, Visibility, Dew Point Temperature, Solar Radiation, Rainfall, Snowfall, Seasons, Holiday, and Functioning Day."
+               ),
+               
+               #Purpose of each tab
+               h3("Tabs"),
+               h4("The Data page will allow the user to have an initial look at the data set, subset it, and save the full data set as a file."),
+               h4("The Data Exploration page will allow the user to explore the data. The user can change and filter the variables; create numerical and graphical summaries; and download the plots."),
+               h4("The Modeling page will fit three supervised learning models. There will be a multiple linear regression model, regression tree, and a random forest model. The three tabs within the page will give information about the modeling approaches, fit the model, and predict the response.")
+      ),
+      
+      tabPanel("Data", fluidPage(
+        #Title
+        titlePanel("Data"),
+        
+        #filter rows by season
+        selectInput("season", "Select a season or all seasons", 
+                    list("All", "Winter", "Spring", "Summer", "Autumn")),
+        
+        #select columns by category 
+        selectInput("col", "Select all columns or by category",
+                    list("Both", "Weather", "Time")),
+        
+        #Download 
+        downloadButton("downloadData", "Download"),
+        
+        #Table outputted
+        tableOutput("table")
+      )),
+      
+      tabPanel("Data Exploration", fluidPage(
+        #Application title
+        titlePanel("Old Faithful Geyser Data"),
+        
+        #Sidebar with a slider input for number of bins
+        sidebarLayout(
+          sidebarPanel(
+            sliderInput("bins", "Number of bins:", min = 1,max = 50, value = 30)
+          ),
+          #table
+          mainPanel(plotOutput("distPlot"))
+        )
+      )),
+      
+      tabPanel("Modeling", tabsetPanel(
+        tabPanel("Modeling Info"),
+        
+        tabPanel("Model Fitting"),
+        
+        tabPanel("Prediction")
+      )
+      )
     )
-  )
-))
+)
